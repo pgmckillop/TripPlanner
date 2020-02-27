628e99c677937747404f122c444892abf5c61339
Paul McKillop <pgmckillop@gmail.com>
2020-02-27 12:42:43 +0100

    Project

---
+++ TripOrganiser/PageProject.xaml.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace TripOrganiser
+{
+    /// <summary>
+    /// Interaction logic for PageProject.xaml
+    /// </summary>
+    public partial class PageProject : Page
+    {
+        public PageProject()
+        {
+            InitializeComponent();
+        }
+    }
+}
